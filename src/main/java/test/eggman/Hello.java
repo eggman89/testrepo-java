a1fcb8219f456bf36f3a160b679e0a3bd829fc68
eggman89 <official.snehasis@gmail.com>
2021-09-06 22:34:59 -0700

    time

---
+++ src/main/java/test/eggman/Hello.java
@@ -0,0 +1,11 @@
+package main.java.test.eggman;
+import org.joda.time.LocalTime;
+public class Hello {
+
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+
+        System.out.println("Main Hello2");
+    }
+}
